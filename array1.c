f8c84ff6f2fa1ab6806ac69151976a7a103093ca
Tehseenmulla <mullat318@gmail.com>
2025-09-14 19:22:12 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+   int size;
+   printf("Enter the array size:");
+   scanf("%d",&size);
+   int arr[size];
+   for(int i=0; i<size; i++){
+      printf("%d\n",arr[0]);
+   }
+}
+
+
